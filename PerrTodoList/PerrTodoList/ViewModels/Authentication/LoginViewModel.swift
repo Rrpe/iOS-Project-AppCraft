43a5e2f97e132a9239d4ecdd13ef030009c61ab6
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-03-05 22:13:51 +0900

    MVVM: LoginViewModel 나누기 작업

---
+++ PerrTodoList/PerrTodoList/ViewModels/Authentication/LoginViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewModel.swift
+//  PerrTodoList
+//
+//  Created by KimJunsoo on 3/5/25.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class LoginViewModel: ObservableObject {
+    
+}
