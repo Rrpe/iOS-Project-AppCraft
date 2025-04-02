371487a765b5976b68a697ff60ee9c7acd96471b
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-02 23:04:42 +0900

    DailyPlanner 프로젝트 생성

---
+++ DailyPlanner/DailyPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyPlanner
+//
+//  Created by KimJunsoo on 4/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
