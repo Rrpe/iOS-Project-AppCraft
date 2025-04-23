e393cf73aadd263189134e1697dee337999c25cd
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-04-23 21:40:10 +0900

    손풀기/복기용 투두리스트 모음집

---
+++ TodoZip/TodoJson/TodoJson/TodoJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoJsonApp.swift
+//  TodoJson
+//
+//  Created by KimJunsoo on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
