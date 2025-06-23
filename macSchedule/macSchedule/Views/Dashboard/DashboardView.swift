21a8654c28074616c3e80103ca1bfdcd3f75db19
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-06-23 23:51:30 +0900

    Initial commit: Project setup

---
+++ macSchedule/macSchedule/Views/Dashboard/DashboardView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  macSchedule
+//
+//  Created by KimJunsoo on 6/23/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DashboardView: View {
+    @Environment(\.modelContext) private var modelContext
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
